79aab8c8486a8d3317a71184d38a8758f74e13bf
Laifengting <laifengting@foxmail.com>
2022-03-14 10:59:55 +0800

    笔记更新

--- dba/常用数据库巡检SQL.sql
+++ dba/常用数据库巡检SQL.sql
@@ -402,3 +402,63 @@ SELECT `table_schema`, `table_name`
 SELECT *
     FROM `information_schema`.`processlist`
     WHERE `user` = 'system user' AND `state` = 'System lock';
+
+
+#### sys 库的使用
+## 索引情况
+#1. 查询冗余索引
+SELECT *
+    FROM `sys`.`schema_redundant_indexes`;
+#2. 查询未使用过的索引
+SELECT *
+    FROM `sys`.`schema_unused_indexes`;
+#3. 查询索引的使用情况
+SELECT `index_name`, `rows_selected`, `rows_inserted`, `rows_updated`, `rows_deleted`
+    FROM `sys`.`schema_index_statistics`
+    WHERE `table_schema` = 'dbname';
+
+
+## 表相关
+# 1. 查询表的访问量
+SELECT `table_schema`, `table_name`, SUM(`io_read_requests` + `io_write_requests`) AS `io`
+    FROM `sys`.`schema_table_statistics`
+    GROUP BY `table_schema`, `table_name`
+    ORDER BY `io` DESC;
+# 2. 查询占用bufferpool较多的表
+SELECT `object_schema`, `object_name`, `allocated`, `data`
+    FROM `sys`.`innodb_buffer_stats_by_table`
+    ORDER BY `allocated`
+    LIMIT 10;
+# 3. 查看表的全表扫描情况
+SELECT *
+    FROM `sys`.`statements_with_full_table_scans`
+    WHERE `db` = 'dbname';
+
+## 语句相关
+#1. 监控SQL执行的频率
+SELECT `db`, `exec_count`, `query`
+    FROM `sys`.`statement_analysis`
+    ORDER BY `exec_count` DESC;
+
+#2. 监控使用了排序的SQL
+SELECT `db`, `exec_count`, `first_seen`, `last_seen`, `query`
+    FROM `sys`.`statements_with_sorting`
+    LIMIT 1;
+
+#3. 监控使用了临时表或者磁盘临时表的SQL
+SELECT `db`, `exec_count`, `tmp_tables`, `tmp_disk_tables`, `query`
+    FROM `sys`.`statement_analysis`
+    WHERE `tmp_tables` > 0 OR `tmp_disk_tables` > 0
+    ORDER BY (`tmp_tables` + `tmp_disk_tables`) DESC;
+
+## IO 相关
+#1. 查看消耗磁盘IO的文件
+SELECT `file`, `avg_read`, `avg_write`, `avg_read` + `avg_write` AS `avg_io`
+    FROM `sys`.`io_global_by_file_by_bytes`
+    ORDER BY `avg_read`
+    LIMIT 10;
+
+## InnoDB 锁相关
+#1. 行锁阻塞情况
+SELECT *
+    FROM `sys`.`innodb_lock_waits`;
